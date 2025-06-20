8cd40be042601b378d387c558ebe040492c5c18a
Christian Sanchez <antulabi@gmail.com>
2025-06-20 13:52:23 -0300

    incremento

--- parcial1.js
+++ parcial1.js
@@ -2,6 +2,7 @@
 parcial de programacion
 Escrito por Charlone Nahuel y Sanchez Christian
 */
+let numero1 = 0;
 var incremento = numero1++; // numero1 = numero1 + 1;
 console.log("El incremento de " + numero1 + " es " + incremento);
 var decremento = numero1--; // numero1 = numero1 - 1;
