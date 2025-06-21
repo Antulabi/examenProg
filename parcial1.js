ead2df84f6990b153298a393c0a4f0717c43b410
Antulabi <antulabi@gmail.com>
2025-06-21 12:14:58 -0300

    Delete parcial1.js

--- parcial1.js
+++
@@ -1,9 +0,0 @@
-/*
-parcial de programacion
-Escrito por Charlone Nahuel y Sanchez Christian
-*/
-let numero1 = 0;
-var incremento = numero1++; // numero1 = numero1 + 1;
-console.log("El incremento de " + numero1 + " es " + incremento);
-var decremento = numero1--; // numero1 = numero1 - 1;
-console.log("El decremento de " + numero1 + " es " + decremento);
